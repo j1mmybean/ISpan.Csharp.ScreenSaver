a7c267a9bda8a06e3be2cac23cf3f818368931a6
j1mmybean <carb4305@gmail.com>
2022-12-29 16:22:13 +0800

    加入專案檔案。

---
+++ ISpan.Csharp.ScreenSaver.win/ScreenSaver.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace ISpan.Csharp.ScreenSaver.win
+{
+    partial class ScreenSaver
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // ScreenSaver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "ScreenSaver";
+            this.Load += new System.EventHandler(this.ScreenSaver_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.ScreenSaver_Paint);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ScreenSaver_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ ISpan.Csharp.ScreenSaver.win/ScreenSaver.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ISpan.Csharp.ScreenSaver.win
+{
+    public partial class ScreenSaver : Form
+    {
+        public ScreenSaver()
+        {
+            InitializeComponent();
+        }
+        private int screenWidth;
+        private int screenHeight;
+        private Bitmap bitmap;
+        private Random random = new Random();
+        private int x = 0;
+        private int y = 0;
+        Rectangle rect = Screen.PrimaryScreen.Bounds;
+        private void ScreenSaver_Load(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Maximized;
+            this.TopMost = true;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.BackColor = Color.Black;
+            this.ShowInTaskbar = false;
+            //获取屏幕的宽度，高度           
+            screenWidth = rect.Width;
+            screenHeight = rect.Height;
+            string currPath = "C:\\Users\\User\\Pictures\\Acer\\Hello_world.bmp";
+            bitmap = new Bitmap(currPath, true);
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            x = random.Next(screenWidth);
+            y = random.Next(screenHeight);
+            if (x + bitmap.Width > screenWidth)
+            {
+                x = screenWidth - bitmap.Width;
+            }
+            if (y + bitmap.Height > screenHeight)
+            {
+                y = screenHeight - bitmap.Height;
+            }
+            this.Invalidate();
+        }
+        private void ScreenSaver_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(bitmap, x, y, bitmap.Width, bitmap.Height);
+        }
+
+        private void ScreenSaver_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.TopMost = false;
+        }
+    }
+}
